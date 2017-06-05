43a530f5c456c0bf0f92a71ad671e180edcc7b4b
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-05 15:23:17 +1000

    add contructor and state, move loggedIn() into App

--- src/App.js
+++ src/App.js
@@ -11,11 +11,20 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register.js';
 
-function loggedIn() {
-    return false;
-}
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            loggedIn: false
+        }
+    }
+
+    loggedIn() {
+        return false;
+    }
+
     render() {
         return (
             <div>
@@ -23,7 +32,7 @@ class App extends Component {
                     <div>
                         <Nav />
                         <Route exact path="/" render={() => (
-                                loggedIn() ? (
+                                this.loggedIn() ? (
                                     <Home />
                                 ) : (
                                     <Redirect to="/login"/>
