d7db51997b75cae916544ee3484f67f9e6de124e
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-11 15:16:19 +1000

    import Modal are factor out repeated code

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,64 +1,41 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 import './Login.css';
+import Modal from './components/Modal'
 
 class Login extends Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
         this.state = {
-            gameCount : 0
         };
     }
 
     render() {
-        axios.get('http://localhost:8080/game/count')
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
         return (
-            <div className="container">
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h4 className="modal-title">Login in</h4>
-                        </div>
-                        <div className="modal-body">
-                            <div className="row">
-                                <div className="col-md-6">
-                                    <div className="well">
-                                        <form method="post" className="form">
-                                            <p>
-                                                <label>Username</label>
-                                                <input type="text" className="form-control" name="name" required="required"/>
-                                            </p>
-                                            <p>
-                                                <label>Password</label>
-                                                <input type="password" className="form-control" name="password" required="required"/>
-                                            </p>
-                                            <p>
-                                                <input type="submit" value="login" className="btn btn-success btn-block"/>
-                                            </p>
-                                        </form>
-                                    </div>
-                                </div>
-                                <div className="col-md-6">
-                                    <p className="lead">Not a registered yet? Click below</p>
-                                    <Link to="/register" className="link-btn"><button className="btn btn-info btn-block">Register</button></Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+            <Modal title='Login'>
+                <div className="col-md-6">
+                    <form method="post" className="form">
+                        <p>
+                            <label>Username</label>
+                            <input type="text" className="form-control" name="name" required="required"/>
+                        </p>
+                        <p>
+                            <label>Password</label>
+                            <input type="password" className="form-control" name="password" required="required"/>
+                        </p>
+                        <p>
+                            <input type="submit" value="login" className="btn btn-success btn-block"/>
+                        </p>
+                    </form>
                 </div>
-            </div>
+                <div className="col-md-6">
+                    <p className="lead">Not signed up yet? Get started here</p>
+                    <Link to="/register" className="link-btn"><button className="btn btn-info btn-block">Register</button></Link>
+                </div>
+            </Modal>
         );
     }
 }
 
 export default Login;
-
-// <p><a href="./register.html" className="btn btn-info btn-block">register now!</a></p>
