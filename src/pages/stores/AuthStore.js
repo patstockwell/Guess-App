673d5133d730e51ee7c0daddf5cc76d39dd6935b
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-15 20:26:26 +1000

    Move Auth into stores folder and use import EventEmitter

---
+++ src/pages/stores/AuthStore.js
@@ -0,0 +1,35 @@
+import { EventEmitter } from 'events'
+
+class AuthStore extends EventEmitter {
+    constructor() {
+        super()
+        this.data = {
+            name: '',
+            id: -1
+        }
+
+    }
+
+    login(newData) {
+        this.data = newData
+    }
+
+    getLogin() {
+        return this.data
+    }
+
+    logout() {
+        this.data = {
+            name: '',
+            id: -1
+        }
+    }
+
+    loggedIn() {
+        return (this.data.id === -1 ? false : true);
+    }
+}
+
+const auth = new AuthStore();
+
+export default auth;
