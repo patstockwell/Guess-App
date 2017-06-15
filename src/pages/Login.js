9f6f14b220f11a2e34b9a8eac26dc0f7c9285ff5
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-15 20:27:39 +1000

    add event handling methods, import AuthStore

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,30 +1,57 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import './Login.css';
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './Login.css'
 import Modal from './components/Modal'
-import Auth from './components/Auth'
+import AuthStore from './stores/AuthStore'
+import axios from 'axios'
 
 class Login extends Component {
 
     constructor(props) {
-        super(props);
-        this.state = {};
+        super(props)
+        this.state = {
+            data: AuthStore.getLogin()
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleReply = this.handleReply.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+        axios.get('http://localhost:8080/player/login', {
+            params: {
+                name: this.state.name,
+                password: this.state.password
+            }
+        }).then(reply => console.log(reply))
+    }
+
+    handleReply(reply) {
+        AuthStore.login(reply.data)
     }
 
     render() {
+        console.log(this.state.data)
         return (
             <div className="container">
                 {this.props.location.justRegistered && (<div className="alert alert-info" role="alert">Success! Account created, login below</div>)}
                 <Modal title='Login'>
                     <div className="col-md-6">
-                        <form method="post" className="form">
+                        <form onSubmit={this.handleSubmit} className="form">
                             <p>
                                 <label>Username</label>
-                                <input type="text" className="form-control" name="name" required="required"/>
+                                <input type="text" className="form-control" name="name" required="required" value={this.state.name} onChange={this.handleChange}/>
                             </p>
                             <p>
                                 <label>Password</label>
-                                <input type="password" className="form-control" name="password" required="required"/>
+                                <input type="password" className="form-control" name="password" required="required" value={this.state.password} onChange={this.handleChange}/>
                             </p>
                             <p>
                                 <input type="submit" value="login" className="btn btn-success btn-block"/>
