22c4eeb491e41882f89893a67960be4858783c79
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-16 18:18:29 +1000

    add welcome message when logged in, add logout redirect

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import AuthStore from './stores/AuthStore'
+import { Redirect } from 'react-router'
 
 class Home extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: true
+        }
+        this.logout = this.logout.bind(this)
+    }
+
+    logout() {
+        this.setState({
+            loggedIn: false
+        })
+    }
+
+    componentWillMount() {
+        AuthStore.on('change', this.logout)
+    }
+
+    componentWillUnmount() {
+        AuthStore.removeListener('change', this.logout)
+    }
+
     render() {
         return (
             <div className="container">
+                {this.props.location.justRegistered && (<div className="alert alert-success" role="alert">Hi {this.props.location.name}, Welcome to the guessing game.</div>)}
                 <h1>Home</h1>
+                {!this.state.loggedIn && (
+                    <Redirect push to={{pathname: "/login"}}/>
+                )}
             </div>
         );
     }
