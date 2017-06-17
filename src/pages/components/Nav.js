7c23785d390ac01e4557d6eeb71fbca14d04d6cd
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-06-17 11:17:43 +1000

    make nav bar smaller, show logout link dynamically

--- src/pages/components/Nav.js
+++ src/pages/components/Nav.js
@@ -5,24 +5,49 @@ import AuthStore from '../stores/AuthStore'
 
 class Nav extends Component {
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            loggedIn: AuthStore.loggedIn(),
+            data: AuthStore.getLogin()
+        }
+        this.updateLogin = this.updateLogin.bind(this)
+    }
+
     logout() {
-        AuthStore.logout();
+        AuthStore.logout()
+    }
+
+    updateLogin() {
+        this.setState({
+            loggedIn: AuthStore.loggedIn(),
+            data: AuthStore.getLogin()
+        })
+    }
+
+    componentWillMount() {
+        AuthStore.on('change', this.updateLogin)
+    }
+
+    componentWillUnmount() {
+        AuthStore.removeListener('change', this.updateLogin)
     }
 
     render() {
         return (
-            <div className="container">
-                <div className="jumbotron">
-                    <div className="container">
-                        <Link to="/"><h1 className="home-heading">Number Guess</h1></Link>
-                        <h4>Current Player: <span id="login"></span></h4>
-                        <div className="login-group">
-                            <Link to="/login"><button className="btn btn-secondary">Login</button></Link>
+            <nav className="navbar navbar-light bg-faded">
+                <div className="container">
+                    <Link to="/"><h1 className="home-heading">Number Guess</h1></Link>
+                    <div className="login-group login-logout">
+                        {this.state.loggedIn && (
                             <button type="button" className="btn btn-success" onClick={this.logout}>Logout</button>
-                        </div>
+                        )}
                     </div>
+                    {this.state.data.name && (
+                        <p className="current-player">Current Player: <span id="login">{this.state.data.name}</span></p>
+                    )}
                 </div>
-            </div>
+            </nav>
         );
     }
 }
